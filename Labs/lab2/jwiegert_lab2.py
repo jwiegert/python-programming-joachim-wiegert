f4e963af5240d0858bb179df805abf204c92cc78
jwiegert <joachimwiegert.astro@gmail.com>
2021-09-07 16:00:39 +0200

    added lab2 pyfile

---
+++ Labs/lab2/jwiegert_lab2.py
@@ -0,0 +1,15 @@
+# First attempt at lab2.
+
+# 1. Load data
+
+# 2. plot all points
+
+# 3. load test points
+
+# 4. compute distance between test points and all points of pickachu and pichu
+#    check if testpoint is closest to pickachu or pichu
+# 5. Print out which points are which.
+
+# 6. Let the use input a test point. Is this part of Pickachu or Pichu?
+
+# 7. ... extra problems...
