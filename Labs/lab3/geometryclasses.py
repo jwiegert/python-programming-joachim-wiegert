f69d7ad5481cbbf69dba9d36101034b55cec7938
jwiegert <joachimwiegert.astro@gmail.com>
2021-09-24 18:06:59 +0200

    started classes

---
+++ Labs/lab3/geometryclasses.py
@@ -0,0 +1,24 @@
+# Different geometry classes for lab3
+
+
+class GeomtryChecks:
+    def __init__(self) -> None:
+        pass
+    
+    def __repr__(self) -> str:
+        return f"This class is a parent to the geometry shapes and error checks the geometry"
+
+class Circle:
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return f"A circle with radius .. and middle at x=.. and y=.."
+
+
+class Rectangle:
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        return f"A rectangle with x-width=, y-width=, "
