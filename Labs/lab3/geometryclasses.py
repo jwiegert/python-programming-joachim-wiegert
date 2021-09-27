a9d19809be9505fa7ba56aa2b706e0f64c8d54ec
jwiegert <joachimwiegert.astro@gmail.com>
2021-09-27 11:42:08 +0200

    minor changes and started lab3

--- Labs/lab3/geometryclasses.py
+++ Labs/lab3/geometryclasses.py
@@ -1,24 +1,63 @@
 # Different geometry classes for lab3
+import numpy as np
 
-
-class GeomtryChecks:
+class GeometryChecks:
     def __init__(self) -> None:
         pass
     
+
+    # print area
+    # f"Area of object is {self.area:.2f}."
+
+    # print circumferance
+
+    # print volume
+
+    # print surface area
+
+    # equality method
+
+    # translate x-y
+
+    # check coords inside
+
+
     def __repr__(self) -> str:
         return f"This class is a parent to the geometry shapes and error checks the geometry"
 
-class Circle:
-    def __init__(self) -> None:
-        pass
+
+
+
+class Circle(GeometryChecks):
+    def __init__(self, radius:float, originx:float, originy: float) -> None:
+        self.radius = radius
+        self.origin = [originx,originy]
+
+    # Use geometrycheck to check inputs
+
+    def area(self) -> float:
+        self.area = np.pi * self.radius**2
+        return self.area
+    
+    def circumferance(self) -> float:
+        self.circum = 2*np.pi*self.radius
+        return self.circum
+    
 
     def __repr__(self) -> str:
-        return f"A circle with radius .. and middle at x=.. and y=.."
+        return f"A circle with radius {self.radius} and middle at x={self.origin[0]} and y={self.origin[1]}"
 
 
-class Rectangle:
+
+
+class Rectangle(GeometryChecks):
     def __init__(self) -> None:
         pass
 
     def __repr__(self) -> str:
         return f"A rectangle with x-width=, y-width=, "
+
+
+# Cube
+
+# Sphere
